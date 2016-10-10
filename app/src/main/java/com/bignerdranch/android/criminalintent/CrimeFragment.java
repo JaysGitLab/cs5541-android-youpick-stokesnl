92af3853d32fbd964db035b0c7ce94d6173b2b76
Nathan Stokes <stokesnl@appstate.edu>
2016-10-10 01:25:27 -0400

    Chapter 12

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,9 +1,12 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.text.DateFormat;
 import java.text.Format;
+import java.util.Date;
 import java.util.UUID;
 
 import android.support.v4.app.Fragment;
@@ -25,12 +28,29 @@ public class CrimeFragment extends Fragment
 {
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
+    private static final int REQUEST_DATE = 0;
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
     private String changed;
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(resultCode != Activity.RESULT_OK)
+        {
+            return;
+        }
+        if(requestCode == REQUEST_DATE) {
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate(mCrime.getDate().toString());
+        }
+    }
+
+    private void updateDate(String text) {
+        mDateButton.setText(text);
+    }
 
     public static CrimeFragment newInstance(UUID crimeId)
     {
@@ -74,7 +94,7 @@ public class CrimeFragment extends Fragment
 
         mDateButton = (Button)v.findViewById(R.id.crime_date);
         changed = DateFormat.getDateInstance().format(mCrime.getDate());
-        mDateButton.setText(changed);
+        updateDate(changed);
         //mDateButton.setEnabled(false);
         mDateButton.setOnClickListener(new View.OnClickListener()
         {
@@ -82,7 +102,9 @@ public class CrimeFragment extends Fragment
             public void onClick(View v)
             {
                 FragmentManager manager = getFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+
                 dialog.show(manager, DIALOG_DATE);
             }
         });
